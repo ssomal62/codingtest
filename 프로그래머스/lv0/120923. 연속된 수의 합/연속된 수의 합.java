658e8b9808ded9f1ab69643a89d56d82e5b76c23
ssomal62 <140372454+ssomal62@users.noreply.github.com>
2023-09-23 10:15:34 +0900

    Update 연속된 수의 합.java
    띄어쓰기 안하면 조^2장님이 뭐라고해서 수정.

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,10 +1,10 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int num, int total) {
-         
-        int s = num%2 == 0 ? -num/2+1 : -num/2 ;
-        int e = num%2 == 0 ? s-1 : s;
-       
-        return IntStream.rangeClosed(s, Math.abs(e)).map(k-> (int)total/num + k).toArray();
+        
+        int s = num % 2 == 0 ? -num / 2 + 1 : -num / 2;
+        int e = num % 2 == 0 ? s - 1 : s;   
+        return IntStream.rangeClosed(s, Math.abs(e)).map(i -> (int) total / num + i).toArray();
+        
     }
 }
