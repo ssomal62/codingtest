2b0d8b7e0fbbe3db5600af62689aa313acb4791d
ssomal62 <140372454+ssomal62@users.noreply.github.com>
2023-10-06 20:35:20 +0900

    [level 0] Title: 저주의 숫자 3, Time: 3.67 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int n) {
+        return IntStream
+                .iterate(0, i -> i + 1)
+                .filter(i-> ((i % 3) != 0) && (!String.valueOf(i).contains("3")))
+                .peek(i-> System.out.println(i))
+                .limit(n)
+                .max()
+                .getAsInt();
+    }
+}
