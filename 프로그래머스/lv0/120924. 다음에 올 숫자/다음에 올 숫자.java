c7eadb19812ae0cb525d004df29843a731dfd29c
ssomal62 <140372454+ssomal62@users.noreply.github.com>
2023-10-07 10:07:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,20 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int[] common) {
+        int ratio = 0; //등비
+        int diff = 0;  //등차
+
+        int e1 = common[0];
+        int e2 = common[1];
+        int e3 = common[2];
+        int el = common[common.length-1];
+
+        if(e2-e1 == e3-e2) {
+            diff = e2 - e1;
+            ratio = 1;
+        } else {
+            ratio = e2 / e1;
+        }
+        return el * ratio + diff;
+    }
+}
