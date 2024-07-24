57a5781a474ad6ee9410a8c084e8e47af48697b3
soyeon <wxy890@gmail.com>
2024-07-24 14:09:10 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 288 ms, Memory: 25788 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Set<String> users = new HashSet<>();
+        int sum = 0;
+        String line;
+
+        for(int i = 0; i < n; i++) {
+            line = br.readLine();
+            if(line.equals("ENTER")){
+                sum += users.size();
+                users.clear();
+            } else {
+                users.add(line);
+            }
+        }
+
+        sum += users.size();
+        System.out.println(sum);
+
+        br.close();
+    }
+}
